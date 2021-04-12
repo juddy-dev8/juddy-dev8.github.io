14e539ea6d56f0d390990721967e0b3997b8d472
juddy-dev8 <67569050+juddy-dev8@users.noreply.github.com>
2021-04-12 13:54:08 +0900

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,46 +1,49 @@
-//importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js');
-//importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js');
-
 // Initialize Firebase
-var firebaseConfig = {
+function initInSw() {
+  // [START messaging_init_in_sw]
+  // Give the service worker access to Firebase Messaging.
+  // Note that you can only use Firebase Messaging here. Other Firebase libraries
+  // are not available in the service worker.
+  importScripts('https://www.gstatic.com/firebasejs/8.3.0/firebase-app.js');
+  importScripts('https://www.gstatic.com/firebasejs/8.3.0/firebase-messaging.js');
+
+  // Initialize the Firebase app in the service worker by passing in
+  // your app's Firebase config object.
+  // https://firebase.google.com/docs/web/setup#config-object
+  firebase.initializeApp({
     apiKey: "AIzaSyBVXG4UfDmB_YPDu8qAnjgyYJuhvgM2VPg",
     authDomain: "inplemap-4b394.firebaseapp.com",
+    databaseURL: 'https://inplemap-4b394.firebaseio.com',
     projectId: "inplemap-4b394",
     storageBucket: "inplemap-4b394.appspot.com",
     messagingSenderId: "246253828554",
     appId: "1:246253828554:web:f79a8d91a9ef042df93733",
     measurementId: "G-SLGEE28FCJ"
-};
+  });
 
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging(); //token 값 알아내기
 
-// Add the public key generated from the console here.
-//messaging.usePublicVapidKey("");
+  // Retrieve an instance of Firebase Messaging so that it can handle background
+  // messages.
+  const messaging = firebase.messaging();
+  // [END messaging_init_in_sw]
+}
 
-messaging.requestPermission()
-.then(function(permission){
-	console.log(permission + " : Have permission");
-	return messaging.getToken();
-})
-.then(function(token){
-	console.log('token : ' + token);
-	//$('#_token').html(token);
-	document.getElementById('_token').innerHTML = token;
-	//$('#_token').html(token);
-})
-.catch(function(arr){
-	console.log("Error Occured");
-	console.log(arr);
-});
+function onBackgroundMessage() {
+  const messaging = firebase.messaging();
 
-messaging.setBackgroundMessageHandler(function(payload){
-
-    const title = "Hello World";
-    const options = {
-            body: payload.data.status
+  // [START messaging_on_background_message]
+  messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+      body: 'Background Message body.',
+      icon: '/firebase-logo.png'
     };
+  
+    self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
+  // [END messaging_on_background_message]
+}
 
-    console.log(payload);
-    return self.registration.showNotification(title,options);
-});
